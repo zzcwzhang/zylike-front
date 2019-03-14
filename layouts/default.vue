7473838e663af5caf05e2fcec0af030432a05de4
apple <448403864@qq.com>
2019-03-14 13:28:07 +0800

    整理界面

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,15 +22,15 @@
     </v-navigation-drawer>
     <v-toolbar :clipped-left="clipped" fixed app>
       <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant"> -->
+      <!--   <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon> -->
+      <!-- </v-btn> -->
+      <!-- <v-btn icon @click.stop="clipped = !clipped"> -->
+      <!--   <v-icon>web</v-icon> -->
+      <!-- </v-btn> -->
+      <!-- <v-btn icon @click.stop="fixed = !fixed"> -->
+      <!--   <v-icon>remove</v-icon> -->
+      <!-- </v-btn> -->
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
@@ -94,7 +94,7 @@
         miniVariant: false,
         right: true,
         rightDrawer: false,
-        title: 'Vuetify.js'
+        title: 'ZYLIKE.COM'
       }
     }
   }
