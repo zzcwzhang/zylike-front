2384f88a3d93ea4b2a3cc15c316a92e37e03a29c
apple <448403864@qq.com>
2019-03-15 03:02:31 +0800

    add menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+	<v-app dark>
+		<v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
 			<v-layout column align-center class="hidden-md-and-down">
 				<v-flex class="mt-5">
 					<v-avatar size="100">
@@ -9,102 +9,137 @@
 				</v-flex>
 				<!-- <p class="subheading mt-1">张远</p> -->
 			</v-layout>
-      <v-list>
-        <v-list-tile v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar :clipped-left="clipped" fixed app>
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant"> -->
-      <!--   <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon> -->
-      <!-- </v-btn> -->
-      <!-- <v-btn icon @click.stop="clipped = !clipped"> -->
-      <!--   <v-icon>web</v-icon> -->
-      <!-- </v-btn> -->
-      <!-- <v-btn icon @click.stop="fixed = !fixed"> -->
-      <!--   <v-icon>remove</v-icon> -->
-      <!-- </v-btn> -->
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
+			<v-list>
+				<v-list-tile v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+					<v-list-tile-action>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title v-text="item.title" />
+					</v-list-tile-content>
+					</v-list-tile>
+				</v-list>
+		</v-navigation-drawer>
+		<v-toolbar :clipped-left="clipped" fixed app>
+			<v-toolbar-side-icon @click="drawer = !drawer" />
+				<!-- <v-btn icon @click.stop="miniVariant = !miniVariant"> -->
+				<!--   <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon> -->
+				<!-- </v-btn> -->
+				<!-- <v-btn icon @click.stop="clipped = !clipped"> -->
+				<!--   <v-icon>web</v-icon> -->
+				<!-- </v-btn> -->
+				<!-- <v-btn icon @click.stop="fixed = !fixed"> -->
+				<!--   <v-icon>remove</v-icon> -->
+				<!-- </v-btn> -->
+				<v-toolbar-title v-text="title" />
+
+					<v-spacer />
+
+						<!-- <v-menu offset-y> -->
+						<!--   <template v-slot:activator="{ on }"> -->
+						<!--     <v-btn -->
+						<!--       color="primary" -->
+						<!--       dark -->
+						<!--       v-on="on" -->
+						<!--       > -->
+						<!--       Dropdown -->
+						<!--     </v-btn> -->
+						<!--   </template> -->
+						<!--   <v-list> -->
+						<!--     <v-list-tile -->
+						<!--       v-for="(item, index) in items" -->
+						<!--       :key="index" -->
+						<!--       @click="" -->
+						<!--       > -->
+						<!--       <v-list-tile-title>{{ item.title }}</v-list-tile-title> -->
+						<!--     </v-list-tile> -->
+						<!--   </v-list> -->
+						<!-- </v-menu> -->
+
+						<v-menu offset-y >
+							<v-btn flat slot="activator" color="grey">
+								<v-icon left>expand_more</v-icon>
+								<span>Menu</span>
+							</v-btn>
+							<v-list>
+								<v-list-tile v-for="link of items" :key="link.title" router :to="link.to">
+									<v-list-tile-title>{{ link.title }}</v-list-tile-title>
+								</v-list-tile>
+							</v-list>
+						</v-menu>
+
+						<v-btn icon @click.stop="rightDrawer = !rightDrawer">
+							<v-icon>menu</v-icon>
+						</v-btn>
+				</v-toolbar>
+				<v-content>
+					<v-container>
+						<nuxt />
+					</v-container>
+				</v-content>
+				<v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+					<v-list>
+						<v-list-tile @click.native="right = !right">
+							<v-list-tile-action>
+								<v-icon light>compare_arrows</v-icon>
+							</v-list-tile-action>
+							<v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+						</v-list-tile>
+					</v-list>
+				</v-navigation-drawer>
+				<v-footer :fixed="fixed" app>
+					<span>&copy; 2019</span>
+				</v-footer>
+	</v-app>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        clipped: false,
-        drawer: false,
-        fixed: false,
-        items: [{
-            icon: 'apps',
-            title: 'Welcome',
-            to: '/'
-          },
-          /* { */
-          /*   icon: 'bubble_chart', */
-          /*   title: 'Inspire', */
-          /*   to: '/inspire' */
-          /* }, */
-          {
-            icon: 'accessibility',
-            title: 'AboutMe',
-            to: '/about'
-          },
-          {
-            icon: 'dashboard',
-            title: 'Recipes',
-            to: '/recipes'
-          },
-          {
-            icon: 'people',
-            title: 'Team',
-            to: '/team'
-          }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'ZYLIKE.COM'
-      }
-    }
-  }
+export default {
+	data() {
+		return {
+			clipped: false,
+			drawer: false,
+			fixed: false,
+			items: [{
+				icon: 'apps',
+				title: 'Welcome',
+				to: '/'
+			},
+			/* { */
+			/*   icon: 'bubble_chart', */
+			/*   title: 'Inspire', */
+			/*   to: '/inspire' */
+			/* }, */
+			{
+				icon: 'accessibility',
+				title: 'AboutMe',
+				to: '/about'
+			},
+			{
+				icon: 'dashboard',
+				title: 'Recipes',
+				to: '/recipes'
+			},
+			{
+				icon: 'people',
+				title: 'Team',
+				to: '/team'
+			}
+			],
+			miniVariant: false,
+			right: true,
+			rightDrawer: false,
+			title: 'ZYLIKE.COM'
+		}
+	}
+}
 </script>
 <style>
-  * {
-    box-sizing: border-box;
-  }
+* {
+	box-sizing: border-box;
+}
 
-  .cfont_kuaile {
-    font-family: 'ZCOOL KuaiLe', cursive, sans-serif;
-  }
+	.cfont_kuaile {
+		font-family: 'ZCOOL KuaiLe', cursive, sans-serif;
+	}
 </style>
