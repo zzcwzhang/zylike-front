31b29b8f95d7bbcb130abd500c4b383d6f5a71f0
apple <448403864@qq.com>
2019-03-15 18:36:07 +0800

    添加百度统计

---
+++ plugins/baidu.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?80921b763690ec22c4b1aaabdba02e61";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
