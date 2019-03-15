019ebe1274dedd239c22a4583062c9b2f5ab35eb
apple <448403864@qq.com>
2019-03-15 11:27:35 +0800

    add date-picker

--- components/Popup.vue
+++ components/Popup.vue
@@ -9,6 +9,32 @@
 				<v-form class="px-3">
 					<v-text-field label="标题" v-model="title" prepend-icon="folder"></v-text-field>
 					<v-textarea label="内容" v-model="content" prepend-icon="edit"></v-textarea>
+
+					<v-menu
+						lazy
+						fooset-y
+						full-width
+						ref="menu"
+						v-model="menu"
+						:return-value.sync="date"
+						>
+						<template v-slot:activator="{ on }">
+							<v-text-field
+								v-model="date"
+								label="Picker in menu"
+								prepend-icon="event"
+								readonly
+								v-on="on"
+								></v-text-field>
+						</template>
+						<v-date-picker v-model="date" no-title scrollable>
+							<v-spacer></v-spacer>
+							<v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+							<v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+						</v-date-picker>
+
+					</v-menu>
+
 					<v-btn flat class="success mx-0 mt-3">添加留言</v-btn>
 				</v-form>
 			</v-card-text>
@@ -23,6 +49,8 @@
 			return {
 				title: '',
 				content: '',
+				date: new Date().toISOString().substr(0, 10),
+				menu: false,
 			}
 		},
 		methods: {
