4dde5d11f96234341b5fcfd8be6a701808cdee5f
apple <448403864@qq.com>
2019-03-17 17:52:50 +0800

    fix md color with 2.7.5

--- pages/article/_mid.vue
+++ pages/article/_mid.vue
@@ -3,15 +3,10 @@
     <v-container>
       <v-layout row justify-center align-center>
         <v-flex sx12 md10>
-					<v-card>
-						<mavon-editor
-						 	v-model="content"
-						 	:subfield="false"
-							codeStyle="dark"
-						 	:toolbarsFlag="false"
-						 	defaultOpen="preview" />
-						</mavon-editor>
-					</v-card>
+          <v-card>
+            <mavon-editor v-model="content" :subfield="false" :toolbarsFlag="false" defaultOpen="preview" />
+            </mavon-editor>
+          </v-card>
         </v-flex>
       </v-layout>
     </v-container>
@@ -19,16 +14,22 @@
 </template>
 
 <script>
-	import { mavonEditor } from 'mavon-editor-uncolor';
-	/* import 'mavon-editor/dist/css/index.css'; */
+  import {
+    mavonEditor
+  } from 'mavon-editor-uncolor';
+  /* import 'mavon-editor/dist/css/index.css'; */
 
   import _ from 'lodash';
   import axios from 'axios';
   export default {
     name: 'normal',
-		components: {
-			mavonEditor,
-		},
+    components: {
+      mavonEditor,
+    },
+    data() {
+      return {
+      };
+    },
     mounted() {},
     async asyncData({
       params,
@@ -36,17 +37,19 @@
     }) {
       const mid = _.get(params, 'mid');
       if (!!mid) {
-				const res = await axios.post('https://manage.zylike.com/api/article/normal', { mid });
-				const resdata = res.data;
-				if (resdata.success&&resdata.data.length > 0) {
+        const res = await axios.post('https://manage.zylike.com/api/article/normal', {
+          mid
+        });
+        const resdata = res.data;
+        if (resdata.success && resdata.data.length > 0) {
           return resdata.data[0];
         } else {
           const {
             info
           } = res.data;
-					return {
-						content: '未查到该文档',
-					};
+          return {
+            content: '未查到该文档',
+          };
         }
       } else {
         return {
@@ -57,14 +60,11 @@
   };
 </script>
 <style>
+  .v-note-wrapper {
+    background: #30303 !important;
+  }
 
-.v-note-wrapper {
-background: #30303 !important;
-}
-.markdown-body {
-background: #30303 !important;
-}
-
-
+  .markdown-body {
+    background: #30303 !important;
+  }
 </style>
-
