a4791a3a9ea8af2e59c1d60ea1f65eb4146c6633
apple <448403864@qq.com>
2019-03-19 11:46:14 +0800

    修复nuxt.config.js配置错误

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,94 +6,106 @@ const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 const axios = require('axios');
 
 module.exports = {
-  mode: 'universal',
+	mode: 'universal',
 
 	// 动态路由
 	generate: {
-		routes: function () {
-		const res = await axios.get('https://manage.zylike.com/api/article/list')
-		.then((res) => res.data).then( (resdata) => {
-			const { data } = resdata;
-			return data.map( md => {
-				return `/article/${md.mid}`;
-			})
-		})
+		routes: function() {
+			return axios.get('https://manage.zylike.com/api/article/list')
+				.then((res) => res.data).then((resdata) => {
+					const {
+						data
+					} = resdata;
+					return data.map(md => {
+						return `/article/${md.mid}`;
+					})
+				})
 		}
-	}
+	},
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: "欢迎来到zylike技术博客",
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: "技术，分享，前端，后端，运维，服务器，人生，阅读，技术英语, 设计" }
-    ],
-		script: [
-			{ src: 'https://hm.baidu.com/hm.js?80921b763690ec22c4b1aaabdba02e61' },
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: "欢迎来到zylike技术博客",
+		meta: [{
+				charset: 'utf-8'
+			},
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1'
+			},
+			{
+				hid: 'description',
+				name: 'description',
+				content: "技术，分享，前端，后端，运维，服务器，人生，阅读，技术英语, 设计"
+			}
 		],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
-      },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe'
-      },
-    ]
-  },
+		script: [{
+			src: 'https://hm.baidu.com/hm.js?80921b763690ec22c4b1aaabdba02e61'
+		}, ],
+		link: [{
+				rel: 'icon',
+				type: 'image/x-icon',
+				href: '/favicon.ico'
+			},
+			{
+				rel: 'stylesheet',
+				href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+			},
+			{
+				rel: 'stylesheet',
+				href: 'https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe'
+			},
+		]
+	},
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: {
+		color: '#fff'
+	},
 
-  /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/style/app.styl'
-  ],
+	/*
+	 ** Global CSS
+	 */
+	css: [
+		'~/assets/style/app.styl'
+	],
 
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/vuetify',
+	/*
+	 ** Plugins to load before mounting the App
+	 */
+	plugins: [
+		'@/plugins/vuetify',
 		'@/plugins/sentry',
 		'@/plugins/baidu',
 		/* { src: '@/plugins/vue-mavon-editor', ssr: true}, // 全局注册,ssr:false保证服务器端不会渲染，从而避免document或window找不到的bug */
-  ],
+	],
 
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: [],
 
-  /*
-  ** Build configuration
-  */
-  build: {
-    transpile: ['vuetify/lib'],
-    plugins: [new VuetifyLoaderPlugin()],
-    loaders: {
-      stylus: {
-        import: ["~assets/style/variables.styl"]
-      }
-    },
-    
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      
-    }
-  }
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		transpile: ['vuetify/lib'],
+		plugins: [new VuetifyLoaderPlugin()],
+		loaders: {
+			stylus: {
+				import: ["~assets/style/variables.styl"]
+			}
+		},
+
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) {
+
+		}
+	}
 }
