8e636c41c9996af96bdf3c1d4805fc20603edcdd
apple <448403864@qq.com>
2019-04-02 16:24:34 +0800

    getter article by theme

--- store/articles.js
+++ store/articles.js
@@ -54,6 +54,91 @@ function getDict(in_subjects) {
 	return dict;
 }
 
+function getByTheme(datas, ntree) {
+	// 文章分层: 遍历所有文章，根据文章subject属性的队列长度，把它放入sortByLayer里的二维数组里
+	// 如果文章的队列长度为N,就把它放在subject[N]里
+	// - 提供路径检测，检测文章主题路径是否与目前subjectTree结构相同, 不同则算作subject损坏，放到0层里
+	// - 提供统计，记录每个主题有多少文章
+	const sortByLayer = [
+		[]
+	]; // 分层二维数组
+	const countRecord = {}; // 统计记录
+	_.forEach(datas, (item) => {
+		if (item.hasOwnProperty('subject') && item.subject.length > 0) {
+			// 获取每篇文章主题长度，即可判断它的是第几层级
+			const subjectLength = item.subject.length;
+			// 保证数组前N-1项,不是空,如果在第N项读取时
+			while (sortByLayer.length < subjectLength + 1) {
+				sortByLayer.push([]);
+			}
+			// 判断路径是否正确, 如果路径不正确则放到0层里
+			const mdSubPath = _.join(item.subject, '.');
+			const isCorrectPath = _.get(ntree, mdSubPath);
+			if (isCorrectPath) { // 当路径正确时
+				sortByLayer[subjectLength].push(item); // 该层的队列添加此文档为新元素
+
+				// 遍历该文章的所有主题，在countRecord对象的属性中，以该主题名字为命名的属性 + 1 作为统计。
+				_.forEach(item.subject, subjectName => {
+					_.set(countRecord, subjectName, (_.get(countRecord, subjectName) || 0) + 1)
+				})
+
+			} else { //当路径不正确时
+				sortByLayer[0].push(item);
+			}
+			/* sortByLayer[ subjectLength ].push(item); */
+
+		} else {
+			sortByLayer[0].push(item);
+		}
+	})
+
+	// 遍历主题树，深度优先遍历
+	const treeWalker = (treeNode, deep) => {
+		_.forEach(treeNode, walk => {
+			// 判断是否有属性'value', 没有->不是主题节点->忽略
+			if (walk.hasOwnProperty('value')) {
+				const {
+					label = '', children = []
+				} = walk;
+				// 添加统计信息
+				_.set(walk, 'mdCount', _.get(countRecord, label) || 0);
+				// 如果该节点孩子数量大于0，先递归子节点
+				if (children.length > 0) {
+					treeWalker(children, deep + 1);
+				}
+				// 获取与目前深度相同的分层等级的文章列表
+				const curLayer = sortByLayer[deep];
+				// 遍历该层的文章，与当前主题命名label相同的则把该文章加入此节点
+				_.forEach(curLayer, md => {
+					// 获取文章主题String，根据层深度
+					const mdSubName = md.subject[deep - 1];
+					// 判断主题是否与文章主题相同
+					if (label == mdSubName) {
+						// 如果该主题节点没有children字段,则新建一个
+						if (!_.isArray(walk['children'])) {
+							walk['children'] = [];
+						}
+						// 把文章压入队列
+						walk['children'].push(md);
+					}
+				})
+			}
+		})
+	}
+	treeWalker(ntree, 1);
+
+	// 把无主题或主题错误的放进去
+	ntree.push({
+		label: '无主题',
+		value: '',
+		children: sortByLayer[0],
+		mdCount: sortByLayer[0].length,
+	})
+
+	return ntree;
+
+}
+
 export const getters = {
 	// 当查询栏不为空时，过滤列表
 	getSearchedList(state) {
@@ -84,5 +169,10 @@ export const getters = {
 			return item;
 		});
 		return all;
+	},
+	getArticlesByTheme(state) {
+		const cloneList = _.cloneDeep(state.list);
+		const cloneSubjects = _.cloneDeep(state.subjects)
+		return getByTheme(cloneList, cloneSubjects);
 	},
 }
