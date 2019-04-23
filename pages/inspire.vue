a45a28f58b6c824c899eeb948706d7ec9dc0b373
apple <448403864@qq.com>
2019-04-23 12:39:56 +0800

    inspire modified

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,6 +1,11 @@
 <template>
-  <v-layout>
-	<v-flex text-xs-center>
+  <v-layout column>
+		<v-flex xs24 text-xs-center>
+			<p>使用codesandbox加载可能比较慢，需要翻墙或加速器。或在github上查看
+			<a href="https://github.com/zzcwzhang/zylike-front">https://github.com/zzcwzhang/zylike-front</a>
+</p>
+		</v-flex>
+	<v-flex xs24 text-xs-center>
 	<iframe src="https://codesandbox.io/s/rm3jlzlmrq" style="width:100%; height:calc(100vh - 160px); border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
 	</v-flex>
   </v-layout>
